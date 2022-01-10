71d532fe22a61824de39a48cc7b22266cc4568bf
MrwanTarik <mrwantarik10@gmail.com>
2022-01-10 20:57:27 +0200

    taskk

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let inp = document.querySelector('input');
 let btn = document.querySelector('button');
 btn.addEventListener('click',() =>{
-    if(inp.value >= 2 && inp.value <= 8){
+    if(inp.value > 2 && inp.value < 8){
         alert(`the selected number is ${inp.value} and its a valid number`);
     } else {
         alert('number invalid');
